230e38b0bef231460b73d30fa41cf59f45651399
Vi-shub <Smsharma3121@gmail.com>
2023-09-17 04:25:51 +0530

    don and rec prof

--- client/src/components/profile/DonProf.jsx
+++ client/src/components/profile/DonProf.jsx
@@ -1,11 +1,9 @@
 import axios from 'axios';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import APIRequests from '../../api';
 
 // Custom card component
 function CustomCard({ children }) {
-
-
-
   return (
     <div className="bg-white p-4 rounded-md shadow-lg mb-4">
       {children}
@@ -13,17 +11,37 @@ function CustomCard({ children }) {
   );
 }
 
-export default function ProfilePage() {
-    useEffect(() => {
-        try{
-            const res = axios.get('http://localhost:5000/api/donar/profile', {
-                email: JSON.parse(localStorage.getItem('profile')).email
-            });
-            console.log(res);
-        } catch(err){
-            console.log(err);
+export default function DonProf() {
+  const [data, setData] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        // Assuming you have a user object with email and userType
+        const user = JSON.parse(localStorage.getItem('profile'));
+        const userType = 'your_user_type'; // Replace with the actual user type
+
+        const res = await APIRequests.getUser({ email: user.email, type: userType });
+
+        if (res.status === 200) {
+          setData(res.data);
+          setLoading(false);
+        } else {
+          // Handle API error response here
+          console.error("Error:", res.data.message);
+          setLoading(false);
         }
-        },[]);
+      } catch (error) {
+        // Handle network or unexpected errors here
+        console.error("Error:", error);
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <section className="bg-gray-200 py-5">
       <div className="container mx-auto">
@@ -32,11 +50,11 @@ export default function ProfilePage() {
             <CustomCard>
               <div className="mb-4">
                 <img
-                  src="https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg?t=st=1694901264~exp=1694901864~hmac=d2e5e2e7551d00ab27b9e2b63b93c727fd2b22c21fb2b85b900db42b8d7267dc"
+                  src={data.avatar || 'https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg?t=st=1694901264~exp=1694901864~hmac=d2e5e2e7551d00ab27b9e2b63b93c727fd2b22c21fb2b85b900db42b8d7267dc'}
                   alt="avatar"
                   className="rounded-full w-32 h-32 mx-auto lg:mx-0"
                 />
-                <p className="text-center text-xl font-bold mt-2">Johnatan Smith</p>
+                <p className="text-center text-xl font-bold mt-2">{data.name || 'Johnatan Smith'}</p>
                 <div className="flex justify-center mb-2">
                   <button className="px-4 py-2 border border-blue-500 text-blue-500 ml-2 hover:bg-blue-100 item-center justify-center">Message</button>
                 </div>
@@ -49,27 +67,27 @@ export default function ProfilePage() {
               <div className="mb-4">
                 <div className="flex justify-between mb-2">
                   <span className="text-gray-600">Full Name</span>
-                  <span className="text-gray-500">Johnatan Smith</span>
+                  <span className="text-gray-500">{data.fullName || 'Johnatan Smith'}</span>
                 </div>
                 <hr />
                 <div className="flex justify-between mb-2">
                   <span className="text-gray-600">Email</span>
-                  <span className="text-gray-500">example@example.com</span>
+                  <span className="text-gray-500">{data.email || 'example@example.com'}</span>
                 </div>
                 <hr />
                 <div className="flex justify-between mb-2">
                   <span className="text-gray-600">Phone</span>
-                  <span className="text-gray-500">(097) 234-5678</span>
+                  <span className="text-gray-500">{data.phone || '(097) 234-5678'}</span>
                 </div>
                 <hr />
                 <div className="flex justify-between mb-2">
                   <span className="text-gray-600">Mobile</span>
-                  <span className="text-gray-500">(098) 765-4321</span>
+                  <span className="text-gray-500">{data.mobile || '(098) 765-4321'}</span>
                 </div>
                 <hr />
                 <div className="flex justify-between">
                   <span className="text-gray-600">Address</span>
-                  <span className="text-gray-500">Bay Area, San Francisco, CA</span>
+                  <span className="text-gray-500">{data.address || 'Bay Area, San Francisco, CA'}</span>
                 </div>
               </div>            
             </CustomCard>
--- client/src/components/profile/RecProf.jsx
+++ client/src/components/profile/RecProf.jsx
@@ -1,11 +1,99 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import APIRequests from '../../api';
 
-const RecProf = () => {
-    return (
-        <div>
-            Recipient
+// Custom card component
+function CustomCard({ children }) {
+  return (
+    <div className="bg-white p-4 rounded-md shadow-lg mb-4">
+      {children}
+    </div>
+  );
+}
+
+export default function RecProf() {
+  const [data, setData] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        // Assuming you have a user object with email and userType
+        const user = JSON.parse(localStorage.getItem('profile'));
+        const userType = 'your_user_type'; // Replace with the actual user type
+
+        const res = await APIRequests.getUser({ email: user.email, type: userType });
+
+        if (res.status === 200) {
+          setData(res.data);
+          setLoading(false);
+        } else {
+          // Handle API error response here
+          console.error("Error:", res.data.message);
+          setLoading(false);
+        }
+      } catch (error) {
+        // Handle network or unexpected errors here
+        console.error("Error:", error);
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <section className="bg-gray-200 py-5">
+      <div className="container mx-auto">
+        <div className="flex flex-col lg:flex-row">
+          <div className="lg:w-1/3 lg:mr-10">
+            <CustomCard>
+              <div className="mb-4">
+                <img
+                  src={data.avatar || 'https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg?t=st=1694901264~exp=1694901864~hmac=d2e5e2e7551d00ab27b9e2b63b93c727fd2b22c21fb2b85b900db42b8d7267dc'}
+                  alt="avatar"
+                  className="rounded-full w-32 h-32 mx-auto lg:mx-0"
+                />
+                <p className="text-center text-xl font-bold mt-2">{data.name || 'Johnatan Smith'}</p>
+                <div className="flex justify-center mb-2">
+                  <button className="px-4 py-2 border border-blue-500 text-blue-500 ml-2 hover:bg-blue-100 item-center justify-center">Message</button>
+                </div>
+              </div>
+            </CustomCard>
+          </div>
+
+          <div className="lg:w-2/3">
+            <CustomCard>
+              <div className="mb-4">
+                <div className="flex justify-between mb-2">
+                  <span className="text-gray-600">Full Name</span>
+                  <span className="text-gray-500">{data.fullName || 'Johnatan Smith'}</span>
+                </div>
+                <hr />
+                <div className="flex justify-between mb-2">
+                  <span className="text-gray-600">Email</span>
+                  <span className="text-gray-500">{data.email || 'example@example.com'}</span>
+                </div>
+                <hr />
+                <div className="flex justify-between mb-2">
+                  <span className="text-gray-600">Phone</span>
+                  <span className="text-gray-500">{data.phone || '(097) 234-5678'}</span>
+                </div>
+                <hr />
+                <div className="flex justify-between mb-2">
+                  <span className="text-gray-600">Mobile</span>
+                  <span className="text-gray-500">{data.mobile || '(098) 765-4321'}</span>
+                </div>
+                <hr />
+                <div className="flex justify-between">
+                  <span className="text-gray-600">Address</span>
+                  <span className="text-gray-500">{data.address || 'Bay Area, San Francisco, CA'}</span>
+                </div>
+              </div>            
+            </CustomCard>
+          </div>
         </div>
-    );
-};
-
-export default RecProf;
+      </div>
+    </section>
+  );
+}
