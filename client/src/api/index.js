1bde5d81c94ab814486a84c2aabfa509e4235681
Stephen Vaz <stephenvaz2003@gmail.com>
2023-09-16 11:15:51 +0530

    base client added

---
+++ client/src/api/index.js
@@ -0,0 +1,40 @@
+import axios from "axios";
+
+const API = axios.create({
+  baseURL: "http://localhost:5000/api",
+});
+
+API.interceptors.request.use((req) => {
+  if (localStorage.getItem("profile")) {
+    req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem("profile")).token
+      }`;
+  }
+  return req;
+});
+
+API.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      console.log("JWT Error -> Going to Login Page")
+      window.location.href = "/auth";
+    }
+    return Promise.reject(error);
+  }
+);
+
+class APIRequests {
+  static async signIn(data) {
+    return await API.post("/auth/login", data);
+  }
+  static async verifyOTP(data) {
+    return await API.post("/auth/otp", data);
+  }
+  static async signUp(data) {
+    return await API.post("/auth/register", data);
+  }
+}
+
+export default APIRequests;
