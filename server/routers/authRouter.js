edb2e5443e8ad28d1ed3f63906fd223f92791957
Stephen Vaz <stephenvaz2003@gmail.com>
2023-09-16 11:09:40 +0530

    base server added

---
+++ server/routers/authRouter.js
@@ -0,0 +1,19 @@
+// Desc: Router for authentication
+import express from 'express';
+import dotenv from 'dotenv';
+import AuthController from '../controllers/authController.js';
+
+dotenv.config()
+
+const authRouter = express.Router();
+
+const aC = new AuthController();
+
+authRouter.post("/register", aC.register);
+
+authRouter.post("/login", aC.login);
+
+authRouter.post("/otp", aC.verifyOTP);
+
+export default authRouter;
+
