1340fcb303593beda066210fe22d02df98a366aa
Siddhesh Sonar <sonarsiddhesh105@gmail.com>
2023-09-17 09:37:51 +0530

    some changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css'
 
 function App() {
+  const profile = JSON.parse(localStorage.getItem('profile'))
+
+  useEffect(() => {
+    if (profile) {
+      window.location.pathname = '/home'
+    }
+    else {
+      window.location.pathname = '/login'
+    }
+  })
 
   return (
     <div className=''>
