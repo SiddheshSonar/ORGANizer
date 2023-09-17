b9555fff2441650f16f2c79412866a569d164abc
Stephen Vaz <stephenvaz2003@gmail.com>
2023-09-17 05:32:55 +0530

    map added

--- mobile/lib/pages/home.dart
+++ mobile/lib/pages/home.dart
@@ -5,7 +5,10 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:mobile/api/api.dart';
+import 'package:mobile/components/home_logo.dart';
+import 'package:mobile/components/home_logged.dart';
 import 'package:mobile/pages/login.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -16,9 +19,22 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   late FirebaseMessaging messaging;
+  RxInt where = 0.obs;
+  late var user;
 
   void handleToken() async {
     final box = GetStorage();
+
+     user = await box.read("user");
+    if (user == null) {
+      // box.write("isIn", 0);
+      where.value = -1;
+    }
+    else {
+      user = jsonDecode(user);
+      where.value = 1;
+    }
+
 
     // Fetch the FCM token.
     String? tokenValue;
@@ -55,41 +71,57 @@ class _HomeState extends State<Home> {
         print("Error updating FCM token in database: $e");
       }
     }
+  }
 
-    final user = box.read('user');
+  _launchURL(double latitude, double longitude) async {
+    final url = Uri.parse(
+        "https://www.google.com/maps/dir/?api=1&destination=$latitude,$longitude");
+
+    if (await canLaunchUrl(url)) {
+      await launchUrl(url);
+    } else {
+      throw 'Could not launch $url';
+    }
   }
 
   @override
   void initState() {
     super.initState();
     messaging = FirebaseMessaging.instance;
+
     handleToken();
     messaging.onTokenRefresh.listen((event) {
-      print("token refresh");
-      print(event);
+      // print("token refresh");
+      // print(event);
+      // TODO: clear localstorage and ask to login again
     });
 
     FirebaseMessaging.onMessage.listen((RemoteMessage event) {});
     FirebaseMessaging.onMessageOpenedApp.listen((message) {
-      print('Message clicked!');
-      print(message.notification!.body);
+      // print('Message clicked!');
+      // print(message);
+      final latlng = message.data["latlng"];
+      final latlngList = latlng.split(",");
+      // print("latlngList: $latlngList");
+      _launchURL(double.parse(latlngList[0]), double.parse(latlngList[1]));
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: TextButton(
-            onPressed: () {
-              // navigate to login page
-              Get.to(
-                () => const Login(),
-                transition: Transition.fadeIn,
-              );
-            },
-            child: const Text("Login")),
-      ),
+
+      body: Obx(() {
+        if (where.value == 0) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        } else if (where.value == -1) {
+          return const HomeLogged(isLogged: false);
+        } else {
+          return const HomeLogged(isLogged: true);
+        }
+      }),
     );
   }
 }
